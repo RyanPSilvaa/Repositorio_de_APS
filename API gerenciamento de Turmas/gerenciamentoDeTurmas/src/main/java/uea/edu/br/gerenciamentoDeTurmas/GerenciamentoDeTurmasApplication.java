f29c792e25484041bdc0764eb7c0ec734d98c137
RyanPSilvaa <166587298+RyanPSilvaa@users.noreply.github.com>
2024-11-17 16:34:31 -0300

    adicionei novos arquivos

---
+++ API gerenciamento de Turmas/gerenciamentoDeTurmas/src/main/java/uea/edu/br/gerenciamentoDeTurmas/GerenciamentoDeTurmasApplication.java
@@ -0,0 +1,13 @@
+package uea.edu.br.gerenciamentoDeTurmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeTurmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeTurmasApplication.class, args);
+	}
+
+}
---
+++ API gerenciamento de Turmas/gerenciamentoDeTurmas/src/test/java/uea/edu/br/gerenciamentoDeTurmas/GerenciamentoDeTurmasApplicationTests.java
@@ -0,0 +1,13 @@
+package uea.edu.br.gerenciamentoDeTurmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeTurmasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
