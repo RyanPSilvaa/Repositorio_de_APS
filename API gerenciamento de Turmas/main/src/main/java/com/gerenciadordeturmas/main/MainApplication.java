1b73fb60a6a764fb0b7f680550f1b1acdbde0811
RyanPSilvaa <166587298+RyanPSilvaa@users.noreply.github.com>
2024-11-19 23:12:12 -0300

    Removi antigos arquivos e adicionei novos

--- API gerenciamento de Turmas/main/src/main/java/com/gerenciadordeturmas/main/MainApplication.java
+++ API gerenciamento de Turmas/main/src/main/java/com/gerenciadordeturmas/main/MainApplication.java
@@ -1,13 +1,13 @@
-package com.universidade.api.gerenciadorDeTurmas;
+package com.gerenciadordeturmas.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GerenciadorDeTurmasApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GerenciadorDeTurmasApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
