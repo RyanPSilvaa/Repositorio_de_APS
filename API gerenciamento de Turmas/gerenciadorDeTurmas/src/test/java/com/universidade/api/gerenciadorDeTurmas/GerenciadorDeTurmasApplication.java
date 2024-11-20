a8c28fa089f7b5ee8f63ec8a00782fde190e9486
RyanPSilvaa <166587298+RyanPSilvaa@users.noreply.github.com>
2024-11-19 22:32:42 -0300

    Atualizei tudo

---
+++ API gerenciamento de Turmas/gerenciadorDeTurmas/src/test/java/com/universidade/api/gerenciadorDeTurmas/GerenciadorDeTurmasApplication.java
@@ -0,0 +1,13 @@
+package com.universidade.api.gerenciadorDeTurmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeTurmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeTurmasApplication.class, args);
+	}
+
+}
