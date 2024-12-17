cca367693759e9764b72e3bc5f28f7bc994d9bbe
RyanPSilvaa <166587298+RyanPSilvaa@users.noreply.github.com>
2024-12-17 19:14:46 -0300

    Atualizei arquivos novamente

---
+++ universidade-gerenciador/src/test/java/universidade/universidade_gerenciador/UniversidadeGerenciadorApplication.java
@@ -0,0 +1,12 @@
+package universidade.universidade_gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversidadeGerenciadorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UniversidadeGerenciadorApplication.class, args);
+    }
+}
+
